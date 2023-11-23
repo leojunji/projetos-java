ea2760e368fdab3f9bf2655e61983cf5625e4896
leojunji <leojunji@outlook.com>
2023-11-22 23:22:20 -0300

    Update V10__insert-into-table-especialidades-.sql

--- api/src/main/resources/db/migration/V10__insert-into-table-especialidades-.sql
+++ api/src/main/resources/db/migration/V10__insert-into-table-especialidades-.sql
@@ -1,29 +1,29 @@
 -- Insert 1
-INSERT INTO `vollmed_api`.`especialidades` (`nome`, `descricao`) VALUES ('Pediatria', 'Um pediatra é um médico especializado no cuidado da saúde infantil.');
+INSERT INTO `vollmed_api`.`especialidades` (`nome`, `descricao`, `ativo`) VALUES ('Pediatria', 'Um pediatra é um médico especializado no cuidado da saúde infantil.', '1');
 
 -- Insert 2
-INSERT INTO `vollmed_api`.`especialidades` (`nome`, `descricao`) VALUES ('Cardiologia', 'Um cardiologista é um médico especializado no cuidado do sistema cardiovascular.');
+INSERT INTO `vollmed_api`.`especialidades` (`nome`, `descricao`, `ativo`) VALUES ('Cardiologia', 'Um cardiologista é um médico especializado no cuidado do sistema cardiovascular.', '1');
 
 -- Insert 3
-INSERT INTO `vollmed_api`.`especialidades` (`nome`, `descricao`) VALUES ('Dermatologia', 'Um dermatologista é um médico especializado no cuidado da pele, cabelos e unhas.');
+INSERT INTO `vollmed_api`.`especialidades` (`nome`, `descricao`, `ativo`) VALUES ('Dermatologia', 'Um dermatologista é um médico especializado no cuidado da pele, cabelos e unhas.', '1');
 
 -- Insert 4
-INSERT INTO `vollmed_api`.`especialidades` (`nome`, `descricao`) VALUES ('Ortopedia', 'Um ortopedista é um médico especializado no cuidado de ossos, músculos e articulações.');
+INSERT INTO `vollmed_api`.`especialidades` (`nome`, `descricao`, `ativo`) VALUES ('Ortopedia', 'Um ortopedista é um médico especializado no cuidado de ossos, músculos e articulações.', '1');
 
 -- Insert 5
-INSERT INTO `vollmed_api`.`especialidades` (`nome`, `descricao`) VALUES ('Oftalmologia', 'Um oftalmologista é um médico especializado no cuidado da saúde ocular e dos olhos.');
+INSERT INTO `vollmed_api`.`especialidades` (`nome`, `descricao`, `ativo`) VALUES ('Oftalmologia', 'Um oftalmologista é um médico especializado no cuidado da saúde ocular e dos olhos.', '1');
 
 -- Insert 6
-INSERT INTO `vollmed_api`.`especialidades` (`nome`, `descricao`) VALUES ('Neurologia', 'Um neurologista é um médico especializado no cuidado do sistema nervoso.');
+INSERT INTO `vollmed_api`.`especialidades` (`nome`, `descricao`, `ativo`) VALUES ('Neurologia', 'Um neurologista é um médico especializado no cuidado do sistema nervoso.', '1');
 
 -- Insert 7
-INSERT INTO `vollmed_api`.`especialidades` (`nome`, `descricao`) VALUES ('Endocrinologia', 'Um endocrinologista é um médico especializado no cuidado das glândulas endócrinas e hormônios.');
+INSERT INTO `vollmed_api`.`especialidades` (`nome`, `descricao`, `ativo`) VALUES ('Endocrinologia', 'Um endocrinologista é um médico especializado no cuidado das glândulas endócrinas e hormônios.', '1');
 
 -- Insert 8
-INSERT INTO `vollmed_api`.`especialidades` (`nome`, `descricao`) VALUES ('Urologia', 'Um urologista é um médico especializado no cuidado do sistema urinário masculino e feminino.');
+INSERT INTO `vollmed_api`.`especialidades` (`nome`, `descricao`, `ativo`) VALUES ('Urologia', 'Um urologista é um médico especializado no cuidado do sistema urinário masculino e feminino.', '1');
 
 -- Insert 9
-INSERT INTO `vollmed_api`.`especialidades` (`nome`, `descricao`) VALUES ('Psiquiatria', 'Um psiquiatra é um médico especializado no cuidado da saúde mental e emocional.');
+INSERT INTO `vollmed_api`.`especialidades` (`nome`, `descricao`, `ativo`) VALUES ('Psiquiatria', 'Um psiquiatra é um médico especializado no cuidado da saúde mental e emocional.', '1');
 
 -- Insert 10
-INSERT INTO `vollmed_api`.`especialidades` (`nome`, `descricao`) VALUES ('Otorrinolaringologia', 'Um otorrinolaringologista é um médico especializado no cuidado dos ouvidos, nariz e garganta.');
+INSERT INTO `vollmed_api`.`especialidades` (`nome`, `descricao`, `ativo`) VALUES ('Otorrinolaringologia', 'Um otorrinolaringologista é um médico especializado no cuidado dos ouvidos, nariz e garganta.', '1');
