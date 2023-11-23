780445911a4145861616a0fb07433d731584fd22
leojunji <leojunji@outlook.com>
2023-11-22 23:16:30 -0300

    Enviando insert de especialidades e medicos

---
+++ api/src/main/resources/db/migration/V10__insert-into-table-especialidades-.sql
@@ -0,0 +1,29 @@
+-- Insert 1
+INSERT INTO `vollmed_api`.`especialidades` (`nome`, `descricao`) VALUES ('Pediatria', 'Um pediatra é um médico especializado no cuidado da saúde infantil.');
+
+-- Insert 2
+INSERT INTO `vollmed_api`.`especialidades` (`nome`, `descricao`) VALUES ('Cardiologia', 'Um cardiologista é um médico especializado no cuidado do sistema cardiovascular.');
+
+-- Insert 3
+INSERT INTO `vollmed_api`.`especialidades` (`nome`, `descricao`) VALUES ('Dermatologia', 'Um dermatologista é um médico especializado no cuidado da pele, cabelos e unhas.');
+
+-- Insert 4
+INSERT INTO `vollmed_api`.`especialidades` (`nome`, `descricao`) VALUES ('Ortopedia', 'Um ortopedista é um médico especializado no cuidado de ossos, músculos e articulações.');
+
+-- Insert 5
+INSERT INTO `vollmed_api`.`especialidades` (`nome`, `descricao`) VALUES ('Oftalmologia', 'Um oftalmologista é um médico especializado no cuidado da saúde ocular e dos olhos.');
+
+-- Insert 6
+INSERT INTO `vollmed_api`.`especialidades` (`nome`, `descricao`) VALUES ('Neurologia', 'Um neurologista é um médico especializado no cuidado do sistema nervoso.');
+
+-- Insert 7
+INSERT INTO `vollmed_api`.`especialidades` (`nome`, `descricao`) VALUES ('Endocrinologia', 'Um endocrinologista é um médico especializado no cuidado das glândulas endócrinas e hormônios.');
+
+-- Insert 8
+INSERT INTO `vollmed_api`.`especialidades` (`nome`, `descricao`) VALUES ('Urologia', 'Um urologista é um médico especializado no cuidado do sistema urinário masculino e feminino.');
+
+-- Insert 9
+INSERT INTO `vollmed_api`.`especialidades` (`nome`, `descricao`) VALUES ('Psiquiatria', 'Um psiquiatra é um médico especializado no cuidado da saúde mental e emocional.');
+
+-- Insert 10
+INSERT INTO `vollmed_api`.`especialidades` (`nome`, `descricao`) VALUES ('Otorrinolaringologia', 'Um otorrinolaringologista é um médico especializado no cuidado dos ouvidos, nariz e garganta.');
---
+++ api/src/main/resources/db/migration/V11__insert-into-table-medicos-.sql
@@ -0,0 +1,29 @@
+-- Insert 1
+INSERT INTO `vollmed_api`.`medicos` (`nome`, `email`, `crm`, `logradouro`, `bairro`, `cep`, `numero`, `uf`, `cidade`, `telefone`, `ativo`, `especialidade_id`) VALUES ('Gabriela', 'gabriela@example.com', '54221', 'Avenida das Flores', 'Centro', '98765432', '456', 'SP', 'São Paulo', '987654321', '1', '1');
+
+-- Insert 2
+INSERT INTO `vollmed_api`.`medicos` (`nome`, `email`, `crm`, `logradouro`, `bairro`, `cep`, `numero`, `uf`, `cidade`, `telefone`, `ativo`, `especialidade_id`) VALUES ('Ricardo', 'ricardo@example.com', '67890', 'Rua da Praia', 'Beira Mar', '56789012', '789', 'BA', 'Salvador', '876543210', '1', '2');
+
+-- Insert 3
+INSERT INTO `vollmed_api`.`medicos` (`nome`, `email`, `crm`, `logradouro`, `bairro`, `cep`, `numero`, `uf`, `cidade`, `telefone`, `ativo`, `especialidade_id`) VALUES ('Amanda', 'amanda@example.com', '54321', 'Avenida do Sol', 'Sol Nascente', '12309876', '234', 'MG', 'Belo Horizonte', '765432109', '1', '3');
+
+-- Insert 4
+INSERT INTO `vollmed_api`.`medicos` (`nome`, `email`, `crm`, `logradouro`, `bairro`, `cep`, `numero`, `uf`, `cidade`, `telefone`, `ativo`, `especialidade_id`) VALUES ('Lucas', 'lucas@example.com', '57789', 'Rua das Montanhas', 'Monte Verde', '98765432', '567', 'RS', 'Porto Alegre', '654321098', '1', '4');
+
+-- Insert 5
+INSERT INTO `vollmed_api`.`medicos` (`nome`, `email`, `crm`, `logradouro`, `bairro`, `cep`, `numero`, `uf`, `cidade`, `telefone`, `ativo`, `especialidade_id`) VALUES ('Camila', 'camila@example.com', '87654', 'Avenida das Árvores', 'Bosque Verde', '23456789', '678', 'PR', 'Curitiba', '210987654', '1', '5');
+
+-- Insert 6
+INSERT INTO `vollmed_api`.`medicos` (`nome`, `email`, `crm`, `logradouro`, `bairro`, `cep`, `numero`, `uf`, `cidade`, `telefone`, `ativo`, `especialidade_id`) VALUES ('Victor', 'victor@example.com', '21456', 'Rua do Mar', 'Costa Azul', '34567890', '789', 'SC', 'Florianópolis', '109876543', '1', '6');
+
+-- Insert 7
+INSERT INTO `vollmed_api`.`medicos` (`nome`, `email`, `crm`, `logradouro`, `bairro`, `cep`, `numero`, `uf`, `cidade`, `telefone`, `ativo`, `especialidade_id`) VALUES ('Isabela', 'isabela@example.com', '54121', 'Avenida dos Ventos', 'Ventania', '45678901', '123', 'RJ', 'Rio de Janeiro', '987654321', '1', '7');
+
+-- Insert 8
+INSERT INTO `vollmed_api`.`medicos` (`nome`, `email`, `crm`, `logradouro`, `bairro`, `cep`, `numero`, `uf`, `cidade`, `telefone`, `ativo`, `especialidade_id`) VALUES ('Felipe', 'felipe@example.com', '56789', 'Rua da Lua', 'Luar', '78901234', '234', 'PE', 'Recife', '876543210', '1', '8');
+
+-- Insert 9
+INSERT INTO `vollmed_api`.`medicos` (`nome`, `email`, `crm`, `logradouro`, `bairro`, `cep`, `numero`, `uf`, `cidade`, `telefone`, `ativo`, `especialidade_id`) VALUES ('Mariana', 'mariana@example.com', '87644', 'Avenida das Estrelas', 'Céu Noturno', '23456789', '567', 'GO', 'Goiânia', '210987654', '1', '9');
+
+-- Insert 10
+INSERT INTO `vollmed_api`.`medicos` (`nome`, `email`, `crm`, `logradouro`, `bairro`, `cep`, `numero`, `uf`, `cidade`, `telefone`, `ativo`, `especialidade_id`) VALUES ('Rodrigo', 'rodrigo@example.com', '23456', 'Rua dos Rios', 'Margem do Rio', '34567890', '789', 'AM', 'Manaus', '109876543', '1', '10');
